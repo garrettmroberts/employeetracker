6feef39cb40d7134883ec2e3a8678735579ac3fc
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-21 11:49:54 -0500

    initialized database and json package

---
+++ db/db.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE DATABASE employeeTrackerDB;
+USE employeeTrackerDB;
+
+-- Initialize employees table
+CREATE TABLE employees (
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10),
+  PRIMARY KEY (id)
+);
+
+-- Initialize roles table
+CREATE TABLE roles (
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary INT(10) NOT NULL,
+  department_id INT(10) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Initializes departments table
+CREATE TABLE departments (
+  id INT(10) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
