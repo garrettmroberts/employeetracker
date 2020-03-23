f0f82246d3980933a2151538956c0f183be1b7e8
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-23 16:49:54 -0500

    user can view different database tables

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Import dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const util = require("util");
 
 // Connection is saved as a variable
 const connection = mysql.createConnection({
@@ -11,121 +12,102 @@ const connection = mysql.createConnection({
   database: "employeeTrackerDB"
 });
 
+// Opens connection to DB
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Server lsitening on id " + connection.threadId);
+  mainLoop();
+})
+
 // Main loop takes user input from loop 
 function mainLoop() {
   inquirer.prompt({
     type: "list",
     name: "userChoice",
     message: "What would you like to do?",
-    choices: ["View all employees", "View employees by department", "View employees by manager", "Add an employee", "Remove an employee", "Update employee role", "Update employee manager", "View all roles", "View all departments", "Exit"]
+    choices: ["View all employees", "View departments", "View roles", "Add a department", "View employees by manager", "Add an employee", "Remove an employee", "Update employee role", "Update employee manager", "View all roles", "View all departments", "Exit"]
   }).then(({ userChoice }) => {
-    if (userChoice === "View all employees") {
-      connect(viewAllEmployees);
-    } else if (userChoice === "View employees by department") {
-      connect(viewEmployeesByDept);
-    } else if (userChoice === "View employees by manager") {
-      connect(viewEmployeesByManager);
-    } else if (userChoice === "Add an employee") {
-      connect(addEmployee);
-    } else if (userChoice === "Remove an employee") {
-      connect(removeEmployee);
-    } else if (userChoice === "Update employee role") {
-      updateEmployeeRole();
-    } else if (userChoice === "Update employee manager") {
-      updateEmployeeManager();
-    } else if (userChoice === "View all roles") {
-      viewAllRoles();
-    } else if (userChoice === "View all departments") {
-      viewAllDepartments();
-    } else if (userChoice === "Exit") {
-      return;
-    };
-  });
-};
-
-// Opens a connection to the database, runs callback function fed into it
-async function connect(callback) {
-  connection.connect(function (err) {
-    if (err) throw err;
-    callback();
-    // connection.end();
-  });
-};
-
-// Not fully functional.  Need to connect role and manager via id
-const viewAllEmployees = function() {
-  connection.query("SELECT * FROM employees", function(err, res) {
-    if (err) throw err;
-    res.forEach(({ first_name, last_name, role_id, manager_id }) => {
-      console.log(`${first_name} ${last_name}`)
-    });
-  });
-};
-
-const viewEmployeesByDept = function() {
-
-};
-
-const viewEmployeesByManager = function() {
-
-};
-
-// Adds employees, however only accepts roles  and managers by their ids.  Does not accept empty manager field.
-const addEmployee = function() {
-  inquirer.prompt([{
-    type: "text",
-    name: "name",
-    message: "What is the new employee's name? (first and last)"
-  },
-  {
-    type: "text",
-    name: "role",
-    message: "What is the new employee's role?"
-  },
-  {
-    type: "text",
-    name: "manager",
-    message: "What is the new employee's manager? (Leave blank if none)"
-  }]).then(({ name, role, manager }) => {
-    name = name.split(" ");
-    const first_name = name[0];
-    const last_name = name[1];
-    connection.query(
-      "INSERT INTO employees SET ?",
-      {
-        first_name: first_name,
-        last_name: last_name,
-        role_id: role,
-        manager_id: manager
-      },
-      function(err, res) {
-        if (err) throw err;
-        console.log(res.affectedRows + " employee inserted!");
+    console.log(userChoice)
+    switch (userChoice) {
+      case "View all employees":
+        viewAllEmployees();
+        break;
+      case "View departments":
+         viewAllDepartments();
+         break;
+      case "View roles":
+        viewAllRoles();
+        break;
+      case "Add a department":
+        addDepartment();
+        break;
+      default:
         connection.end();
-        mainLoop();
-      }
-    );
+    }
+    // if (userChoice === "View all employees") {
+    //   viewAllEmployees();
+    // }
+    // switch(userChoice) {
+    //   case "View all Employees":
+    //     viewAllEmployees();
+    //   case "View employees by department":
+    //     viewEmployeesByDept();
+    // };
+    // if (userChoice === "View all employees") {
+    //   connect(viewAllEmployees);
+    // } else if (userChoice === "View employees by department") {
+    //   connect(viewEmployeesByDept);
+    // } else if (userChoice === "View employees by manager") {
+    //   connect(viewEmployeesByManager);
+    // } else if (userChoice === "Add an employee") {
+    //   connect(addEmployee);
+    // } else if (userChoice === "Remove an employee") {
+    //   connect(removeEmployee);
+    // } else if (userChoice === "Update employee role") {
+    //   updateEmployeeRole();
+    // } else if (userChoice === "Update employee manager") {
+    //   updateEmployeeManager();
+    // } else if (userChoice === "View all roles") {
+    //   viewAllRoles();
+    // } else if (userChoice === "View all departments") {
+    //   viewAllDepartments();
+    // } else if (userChoice === "Exit") {
+    //   connection.end();
+    //   return;
+    // };
   });
 };
 
-const removeEmployee = function() {
 
+// Shows employee list w/ full names and job title
+function viewAllEmployees() {
+  const query = `SELECT employees.first_name, employees.last_name, roles.title
+  FROM employees
+  INNER JOIN roles
+  ON employees.role_id = roles.id`
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainLoop();
+  });
 };
 
-const updateEmployeeRole = function() {
-
+// Shows all Departments
+function viewAllDepartments() {
+  connection.query("SELECT * FROM departments", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainLoop();
+  });
 };
 
-const updateEmployeeManager = function() {
-
+// Displays all current roles
+function viewAllRoles() {
+  connection.query("SELECT * FROM roles", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+    mainLoop();
+  });
 };
 
-const viewAllRoles = function() {
 
-};
-
-const viewAllDepartments = function() {
-
-};
-
-mainLoop()
