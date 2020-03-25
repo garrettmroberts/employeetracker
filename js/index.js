e05e19fd9ed895ab2946a61eb99d51ecdbdb5b04
Garrett Roberts <garrettmroberts@gmail.com>
2020-03-25 16:56:44 -0500

    finished MVP

--- js/index.js
+++ js/index.js
@@ -3,14 +3,13 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 const util = require("util");
 
-mainLoop();
 // Main loop takes user input from loop 
 function mainLoop() {
   inquirer.prompt({
     type: "list",
     name: "userChoice",
     message: "What would you like to do?",
-    choices: ["View all employees", "View departments", "View roles", "Add a department", "Add a role", "Add an employee", "View employees by manager", "Add an employee", "Remove an employee", "Update employee role", "Update employee manager", "View all roles", "View all departments", "Exit"]
+    choices: ["View all employees", "View departments", "View roles", "Add a department", "Add a role", "Add an employee", "Update Employee Role", "Exit"]
   }).then(({ userChoice }) => {
     console.log(userChoice)
     switch (userChoice) {
@@ -31,6 +30,9 @@ function mainLoop() {
         break;
       case "Add an employee":
         collectAddEmployeeInfo();
+        break;
+      case "Update Employee Role":
+        updateEmployeeRole();
         break;
       default:
         connection.end();
@@ -62,19 +64,38 @@ async function generateDepartmentArray() {
   return result.map(dept => dept.name);
 };
 
-// Generates employee array from database + a (none)
-async function generateEmployeeArray() {
+// Generates manager array from database + a (none)
+async function generateManagerArray() {
   const result = await queryAsync("SELECT first_name, last_name FROM employees");
   var resultArray = result.map(employee => `${employee.first_name} ${employee.last_name}`);
   resultArray.push("(none)");
   return resultArray;
 };
 
+// Generates employee array from database
+async function generateEmployeeArray() {
+  const result = await queryAsync("SELECT first_name, last_name FROM employees");
+  var resultArray = result.map(employee => `${employee.first_name} ${employee.last_name}`);
+  return resultArray;
+}
+
 // Generates all roles available in a database
 async function generateRolesArray() {
   const result = await queryAsync("SELECT title FROM roles");
   return result.map(role => role.title)
 };
+
+// Helper function gets employee ID by first and last name params
+async function getEmployeeIDByName(first_name, last_name) {
+  const result = await queryAsync(`SELECT id FROM employees WHERE first_name="${first_name}" && last_name="${last_name}"`);
+  return result[0].id
+};
+
+// Helper function gets job id by title param
+async function getRoleIDByTitle(title) {
+  const result = await queryAsync(`SELECT id FROM roles WHERE title="${title}"`);
+  return result[0].id;
+}
 
 // Shows employee list w/ full names and job title
 function viewAllEmployees() {
@@ -156,10 +177,10 @@ async function addRole() {
   });
 };
 
-// Needs a bit of work.  Scoping issues.
+// Adds a new employee, either with or without a manager
 async function collectAddEmployeeInfo() {
   var roles = await generateRolesArray();
-  var managers = await generateEmployeeArray();
+  var managers = await generateManagerArray();
   await inquirer.prompt([
     {
       type: "text",
@@ -192,24 +213,7 @@ async function collectAddEmployeeInfo() {
       });
     } else {
       addEmployeeWithoutManager(first_name, last_name, role);
-    }
-    // var role_id;
-    // var manager_id;
-    // queryAsync(`SELECT id FROM roles WHERE title="${role}"`, (err, res) => {
-    //   if (err) throw err;
-    //   role_id = res[0].id;
-    // });
-    // if (manager != "(none)") {
-    //   var managerFirstName = manager.split(" ")[0];
-    //   var managerLastName = manager.split(" ")[1];
-    //   queryAsync(`SELECT id FROM employees WHERE first_name="${managerFirstName}" && last_name="${managerLastName}"`, (err, res) => {
-    //     if (err) throw err;
-    //     manager_id = res[0].id;
-    //   });
-    //   addEmployeeWithManager(first_name, last_name, role_id, manager_id);
-    // } else {
-    //   addEmployeeWithoutManager();
-    // }
+    };
   });
 };
 
@@ -233,4 +237,29 @@ function addEmployeeWithoutManager(first_name, last_name, role) {
   });
 };
 
+async function updateEmployeeRole() {
+  var employeeList = await generateEmployeeArray()
+  inquirer.prompt({
+    type: "list",
+    name: "employee",
+    message: "Choose an employee",
+    choices: employeeList
+  }).then(async ({ employee }) => {
+    var first_name = employee.split(" ")[0];
+    var last_name = employee.split(" ")[1];
+    var employee_id = await getEmployeeIDByName(first_name, last_name);
+    var rolesList = await generateRolesArray();
+    inquirer.prompt({
+      type: "list",
+      name: "role",
+      message: "What is the new role?",
+      choices: rolesList
+    }).then(async ({ role }) => {
+      var role_id = await getRoleIDByTitle(role)
+      queryAsync(`UPDATE employees SET role_id="${role_id}" WHERE id="${employee_id}"`);
+      mainLoop();
+    })
+  });
+};
 
+mainLoop();
